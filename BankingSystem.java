980a9ce31b389067827f41a077464a91b0e28598
calvin-alvrz <calvin_alvarez@outlook.com>
2023-08-11 21:22:08 +1200

    v0.2

--- BankingSystem.java
+++ BankingSystem.java
@@ -5,7 +5,9 @@ public class BankingSystem {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter your \'Name\'' and \'Customer ID\'' to access your bank account");
+        System.out.println("Name:");
         String name = keyboard.nextLine();
+        System.out.println("Customer ID (e.g. 123123)");
         String customerId = keyboard.nextLine();
         BankAccount obj1 = new BankAccount(name, customerId);
         obj1.menu();
